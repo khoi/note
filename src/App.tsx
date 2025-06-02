b1774911bec47275bfd71f9eea2e1cb760edb6df
khoi <git@khoi.io>
2025-06-02 19:58:11 +0700

    Biome format

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import MinimalTiptapOne from "./Editor";
 import { cn } from "./lib/utils";
 
 function App() {
-  return (
-    <div className="h-screen w-screen">
-      <MinimalTiptapOne
-        throttleDelay={1000}
-        className={cn("h-full w-full rounded-xl")}
-        editorContentClassName="overflow-auto h-full"
-        output="html"
-        placeholder="Comment here..."
-        editable={true}
-        editorClassName="focus:outline-hidden px-5 py-4 h-full"
-      />
-    </div>
-  );
+	return (
+		<div className="h-screen w-screen">
+			<MinimalTiptapOne
+				throttleDelay={1000}
+				className={cn("h-full w-full rounded-xl")}
+				editorContentClassName="overflow-auto h-full"
+				output="html"
+				placeholder="Comment here..."
+				editable={true}
+				editorClassName="focus:outline-hidden px-5 py-4 h-full"
+			/>
+		</div>
+	);
 }
 
 export default App;
